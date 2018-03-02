0867e95546e1eea00d716b7e8f89da01bcaa8bc5
Steve Lasker <StevenLasker@Hotmail.com>
2018-03-01 20:05:31 -0800

    Add debugging info to about

--- HelloWorld/Controllers/HomeController.cs
+++ HelloWorld/Controllers/HomeController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Diagnostics;
 using HelloWorld.Models;
 
 namespace HelloWorld.Controllers
@@ -17,7 +20,24 @@ namespace HelloWorld.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            // Debugging Information 
+            ViewData["Message"] = "Debugging Info.";
+            ViewData["REGISTRYIP"] = System.Net.Dns.GetHostAddresses("stevelasdemos.azurecr.io")[0].ToString();
+
+            ViewData["HOSTNAME"] = Environment.GetEnvironmentVariable("COMPUTERNAME") ??
+                                            Environment.GetEnvironmentVariable("HOSTNAME");
+            ViewData["OSARCHITECTURE"] = RuntimeInformation.OSArchitecture;
+            ViewData["OSDESCRIPTION"] = RuntimeInformation.OSDescription;
+            ViewData["PROCESSARCHITECTURE"] = RuntimeInformation.ProcessArchitecture;
+            ViewData["FRAMEWORKDESCRIPTION"] = RuntimeInformation.FrameworkDescription;
+            ViewData["ASPNETCOREPACKAGEVERSION"] = Environment.GetEnvironmentVariable("ASPNETCORE_PKG_VERSION");
+            ViewData["ASPNETCORE_ENVIRONMENT"] = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            StringBuilder envVars = new StringBuilder();
+            foreach (DictionaryEntry de in Environment.GetEnvironmentVariables())
+                envVars.Append(string.Format("<strong>{0}</strong>:{1}<br \\>", de.Key, de.Value));
+
+            ViewData["ENV_VARS"] = envVars.ToString();
 
             return View();
         }
